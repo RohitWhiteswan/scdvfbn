e9a40b8cc9007a7ac32285c385c289eddca97959
RohitWhiteswan <rsingh@whiteswansecurity.com>
2024-07-29 21:59:11 +0530

    ergfhnmh

--- abc.js
+++ abc.js
@@ -2,3 +2,11 @@ console.log("Prashant")
 console.log("rohit")
 
 console.log("suraj");
+// gnhgtrthgtretghnhtr
+
+
+const a = () =>"Rohit is a good boy";
+
+for(let i = 1;i<5; i++){
+    console.log(a());
+}
